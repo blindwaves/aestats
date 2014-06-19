559c39b07688008bfe910fe1de7ac0e05e35713c
Gareth <gareth@crashtesttheory.com>
2014-06-19 20:03:25 +0800

    Correct redirection method.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -7,7 +7,7 @@ class HomeController extends BaseController {
     public function getIndex()
     {
         if (strcmp($_SERVER['HTTP_HOST'], '128.199.233.47') === 0) {
-            return Redirect::to('http://fundnel.crashtesttheory.com/');
+            return Redirect::away('http://fundnel.crashtesttheory.com/');
         } else {
             return View::make('home/index');
         }
