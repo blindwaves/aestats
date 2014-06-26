52d6db34672df74abece0ef9a5ec64084513271b
Gareth <gareth@crashtesttheory.com>
2014-06-26 17:06:16 +0800

    Add index to the column value for faster searching.

---
+++ app/database/migrations/2014_06_26_090036_histories_add_index_value.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class HistoriesAddIndexValue extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('histories', function($t) {
+			$t->index('value');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('histories', function($t) {
+			$t->dropIndex('value');
+		});
+	}
+
+}
